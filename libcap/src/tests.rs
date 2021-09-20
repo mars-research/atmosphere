e30cf58d71a0da45afe7ce5e3fc1887d52fcc60b
xiangdong <xiangdc@bu.edu>
2021-09-20 13:29:49 -0700

    More retype and invaild CapPointer tests.

--- libcap/src/tests.rs
+++ libcap/src/tests.rs
@@ -80,3 +80,66 @@ fn test_untyped_retype() {
         .as_cpu()
         .expect("Slot 1 must be a Cpu");
 }
+
+#[test]
+fn test_untyped_retype_all() {
+    let cspace = AllocCSpace::new();
+    let cnode = cspace.root_object();
+
+    for i in 1..256{
+        let untyped = cnode.get(0).unwrap().as_ref().unwrap().as_untyped().unwrap();
+
+        let destination = {
+            let first_layer = 1;
+            ((first_layer << (32 - 8))*i).into()
+        };
+        untyped.retype(CapType::Cpu, &cspace, destination)
+            .expect("Failed to retype");
+
+        //println!("Itr {} CSpace: {}", i, *cspace);
+
+        match cnode.get(i as usize){
+            Some(cslot) =>{
+                match cslot.as_ref(){
+                    Some(cslot_ref) =>{
+                        match cslot_ref.as_cpu(){
+                            Some(_) => {},
+                            None => panic!("Slot {} must be a Cpu",i),
+                        };
+                    },
+                    None => panic!("Slot {} must not be empty",i),
+                };
+            },
+            None => panic!("Slot {} must exist",i),
+        };
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_untyped_retype_self() {
+    let cspace = AllocCSpace::new();
+    let cnode = cspace.root_object();
+
+    let untyped = cnode.get(0).unwrap().as_ref().unwrap().as_untyped().unwrap();
+    let destination = {
+        let first_layer = 1;
+        ((first_layer << (32 - 9))).into()
+    };
+    untyped.retype(CapType::Cpu, &cspace, destination)
+        .expect("Failed to retype");
+}
+
+#[test]
+#[should_panic]
+fn test_invaild_cappointer(){
+    let cspace = AllocCSpace::new();
+
+    let destination = {
+        let first_layer = 1;
+        (first_layer << (32 - 8)).into()
+    };
+
+    cspace.resolve(destination).unwrap().as_ref()
+        .expect("Slot 1 must not be vaild");
+}
