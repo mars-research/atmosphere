04b24662cb630f9a44543b7ba9991a16878b05d3
Vikram Narayanan <vikram186@gmail.com>
2024-02-23 13:13:48 -0700

    dom0: wire up bumpallocator

---
+++ dom0/src/allocator.rs
@@ -0,0 +1,108 @@
+//! Simple bump allocator.
+//!
+//! Since our only purpose is to load the microkernel and the
+//! initial task, we can keep this simple.
+
+use super::println;
+use core::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+#[global_allocator]
+pub static mut ALLOCATOR: BumpAllocator = BumpAllocator {
+    base: 0 as *mut u8,
+    size: 0,
+    watermark: AtomicUsize::new(0),
+};
+
+pub struct BumpAllocator {
+    base: *mut u8,
+    size: usize,
+    watermark: AtomicUsize,
+}
+
+unsafe impl Sync for BumpAllocator {}
+
+unsafe impl GlobalAlloc for BumpAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut allocated = ptr::null_mut();
+
+        self.watermark
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |mut watermark| {
+                let mut required_size = layout.size();
+                let mut obj_start = self.base.addr() + watermark;
+                let mask = layout.align() - 1;
+
+                if obj_start & mask != 0 {
+                    let aligned = (obj_start | mask) + 1;
+                    required_size += aligned - obj_start;
+                    obj_start = aligned;
+                }
+
+                if watermark + required_size > self.size {
+                    log::error!("Out of memory");
+                    return None;
+                }
+
+                // Sizes larger than isize::MAX are dangerous in unsafe Rust because
+                // methods in the pointer primitive type make the assumption that
+                // any offset never overflows an isize, even when the method accepts
+                // usize as the argument.
+                //
+                // We do not allow such allocations in Atmosphere.
+                //
+                // <https://doc.rust-lang.org/std/primitive.pointer.html#method.add>
+                if required_size > isize::MAX as usize {
+                    log::error!(
+                        "Allocation size {} ({} after round-up) too big",
+                        layout.size(),
+                        required_size
+                    );
+                    return None;
+                }
+
+                allocated = self.base.with_addr(obj_start);
+                watermark += required_size;
+                Some(watermark)
+            })
+            .unwrap();
+
+        allocated
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        // it's a bump allocator :)
+    }
+}
+
+impl BumpAllocator {
+    /// Reserves a region, returning a new BumpAllocator.
+    pub fn reserve(&mut self, size: usize) -> BumpAllocator {
+        let layout = Layout::from_size_align(size, 4096).expect("Invalid layout");
+        let base = unsafe { self.alloc(layout) };
+
+        Self {
+            base,
+            size,
+            watermark: AtomicUsize::new(0),
+        }
+    }
+
+    /// Returns the base of the allocation.
+    pub fn base(&self) -> *mut u8 {
+        self.base
+    }
+}
+
+/// Initializes the allocator.
+///
+/// This is not thread-safe and should only be called once.
+pub unsafe fn init(base: *mut u8, size: usize) {
+    ALLOCATOR.base = base;
+    ALLOCATOR.size = size;
+    log::info!(
+        "Init allocator with base {:x} and size {}",
+        base as u64,
+        size
+    )
+}
--- dom0/src/main.rs
+++ dom0/src/main.rs
@@ -1,8 +1,17 @@
 #![no_std]
 #![no_main]
-#![feature(start)]
+#![feature(start, strict_provenance)]
 
+extern crate alloc;
+
+use alloc::vec::Vec;
 use core::panic::PanicInfo;
+
+pub use log::info as println;
+
+mod allocator;
+
+static mut MEMORY_REGION: [u8; 4096] = [0u8; 4096];
 
 #[start]
 #[no_mangle]
@@ -11,6 +20,7 @@ fn main() -> isize {
     log::info!("hello {}", "world");
 
     unsafe {
+        allocator::init(&mut MEMORY_REGION as *mut [u8; 4096] as *mut u8, 4096);
         asys::sys_print("meow".as_ptr(), 4);
         log::info!("sys_mmap {:?}", asys::sys_mmap(0xA000000000, 0x0000_0000_0000_0002u64 as usize, 20));
     }
@@ -24,6 +34,12 @@ fn main() -> isize {
     //     }
     //     log::info!("*{:x?} = {:x?}", (0xA000000000usize + i * 4096), user_value);
     // }
+
+    let mut v1: Vec<u64> = Vec::with_capacity(10);
+
+    for i in 0..10 {
+        v1.push(0xdead);
+    }
 
     loop {}
 }
